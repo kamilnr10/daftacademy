c6c2bd4e0e5781555abc122c03f5c0a249e57453
Kamilnr10 <kamil.nr10@gmail.com>
2020-04-20 22:06:34 +0200

    Task 1 to check

---
+++ src/bro.js
@@ -0,0 +1,7 @@
+const bro = greeting => {
+    return `${greeting}, dude`
+}
+
+export {
+    bro
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import {
+    bro
+} from "./bro";
+import "./styles/style.sass";
+import fn from "./script"
+
+console.log(bro("Kmil"));
+fn()
